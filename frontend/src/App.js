01f03209a75447aece7d076b7f6f1491a8187bc4
gabrielgnasc <gabrielgomeslego@gmail.com>
2020-03-18 20:47:41 -0300

    Adição da tela de login

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from  './pages/login';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App-header">
+      <div className="info-login-bg" ></div>
+      <Login></Login>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/cabecalho.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './cabecalho.css';
+
+function Cabecalho() {
+  return (
+    <div className="App-h">
+        <h2 className="logo" >QR-ID</h2>
+    </div>
+  );
+}
+
+export default Cabecalho;
